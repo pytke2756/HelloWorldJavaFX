d6dfab1a8db1ef6ffa56d92b590a1ac023e4b601
Levente Ibrányi <ibranyilevente@gmail.com>
2021-11-24 16:36:32 +0100

    ListView example

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,34 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class Controller {
 
-    public Label welcomeText;
-    public TextField nevBevitelMezo;
+    @FXML
+    private Label eletKorLabel;
+    @FXML
+    private TextField nevBevitelMezo;
+    @FXML
+    private Spinner<Integer> eletkorBevitel;
+    @FXML
+    private ListView<Ember> emberLista;
+
+    public void felvetelClick(){
+        String nev = nevBevitelMezo.getText();
+        int eletkor = eletkorBevitel.getValue();
+        Ember ember = new Ember(nev, eletkor);
+        emberLista.getItems().add(ember);
+    }
 
     public void helloClick(ActionEvent actionEvent) {
         String nev = nevBevitelMezo.getText();
         Alert felugroAblak = new Alert(Alert.AlertType.NONE, "Hello "+nev, ButtonType.OK);
         /*felugroAblak.setContentText("Hello "+nev);
         felugroAblak.getButtonTypes().add(ButtonType.OK)*/
-        felugroAblak.show();
+        felugroAblak.showAndWait();
+        eletKorLabel.setText("Hello");
 
     }
 }
