e5ff8bbf26297c7be98b93a35980aab0dd19f524
Levente Ibrányi <ibranyilevente@gmail.com>
2021-11-24 15:40:13 +0100

    Controller.java welcome message with Alert

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,22 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     public Label welcomeText;
+    public TextField nevBevitelMezo;
 
     public void helloClick(ActionEvent actionEvent) {
-        welcomeText.setText("Köszöntöm a 14.S osztályt az elsü JavaFx gyakorlaton!");
+        String nev = nevBevitelMezo.getText();
+        Alert felugroAblak = new Alert(Alert.AlertType.NONE, "Hello "+nev, ButtonType.OK);
+        /*felugroAblak.setContentText("Hello "+nev);
+        felugroAblak.getButtonTypes().add(ButtonType.OK)*/
+        felugroAblak.show();
+
     }
 }
