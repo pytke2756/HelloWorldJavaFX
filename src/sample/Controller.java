46e0f9bd80415b4606f43385236c189cdb3c5de4
Levente Ibrányi <ibranyilevente@gmail.com>
2021-11-24 15:09:11 +0100

    helloClick action

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    public Label welcomeText;
+
+    public void helloClick(ActionEvent actionEvent) {
+        welcomeText.setText("Köszöntöm a 14.S osztályt az elsü JavaFx gyakorlaton!");
+    }
 }
